6dfe6bfa7192fc1016bac5a40a7df91723a77d3c
Ruzbihan <ruzbihanmzm@gmail.com>
2024-05-02 06:05:57 +0530

    Project Initialize and created API controller

---
+++ Bulky.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulky.DataAccess.Repository.IRepository
+{
+    internal interface IRepository<T> where T : class
+    {
+        //T - Category
+        IEnumerable<T> GetAll(); //get all
+        T Get(Expression<Func<T, bool>> filter); //get one
+        void Add(T entity);
+        void Update(T entity);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entity);
+    }
+}
