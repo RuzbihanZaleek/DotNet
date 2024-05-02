d2638f81c3de6e4ba810c359f3491030048a5405
Ruzbihan <ruzbihanmzm@gmail.com>
2024-05-02 06:21:03 +0530

    DTO

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -1,20 +1,22 @@
 ﻿using MagicVilla_VillaAPI.Models;
+using MagicVilla_VillaAPI.Models.Dto;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MagicVilla_VillaAPI.Controllers
 {
     [Route("api/VillaAPI")]
+    // [Route("api/[controller]")] //this will take the controller name (VillaAPI) automatically 
     [ApiController]
     public class VillaAPIController : ControllerBase
     {
         [HttpGet]
-        public IEnumerable<Villa> GetVillas() 
+        public IEnumerable<VillaDto> GetVillas() 
         {
-            return new List<Villa>
+            return new List<VillaDto>
             {
-                new Villa{ Id=1, Name="Pool Villa"},
-                new Villa{ Id=2, Name="Beach Villa"}
+                new VillaDto{ Id=1, Name="Pool Villa"},
+                new VillaDto{ Id=2, Name="Beach Villa"}
             };
         }
     }
